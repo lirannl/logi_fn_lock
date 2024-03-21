3d8c12946d28645338cd95fb5d45678de01d1a41
Liran Piade <liranpiade@gmail.com>
2024-03-22 00:52:35 +1000

    Make the rust executable

--- fn_activator/src/main.rs
+++ fn_activator/src/main.rs
@@ -1,3 +1,43 @@
+use anyhow::Result;
+use hidapi::HidApi;
+
+struct Keyboard {
+    pub vendor: u16,
+    pub device_id: u16,
+    pub fkey_sequence: Vec<u8>,
+}
+
+const LOGITECH_VENDOR_ID: u16 = 0x046d;
+
 fn main() {
-    println!("Hello, world!");
+    _main().unwrap();
+}
+
+fn _main() -> Result<()> {
+    let hid = HidApi::new()?;
+    let vec: Vec<Keyboard> = vec![
+        // K380
+        Keyboard {
+            vendor: LOGITECH_VENDOR_ID,
+            device_id: 0xb342,
+            fkey_sequence: vec![0x10, 0xff, 0x0b, 0x1e, 0x00, 0x00, 0x00],
+        },
+        // K480
+        Keyboard {
+            vendor: LOGITECH_VENDOR_ID,
+            device_id: 0xb33d,
+            fkey_sequence: vec![0x10, 0xff, 0x0b, 0x1e, 0x00, 0x00, 0x00],
+        },
+    ];
+    for (device, sequence) in vec.into_iter().filter_map(|d| {
+        Some((
+            hid.open(d.vendor, d.device_id)
+                .map_err(|err| println!("{err:#?}"))
+                .ok()?,
+            d.fkey_sequence,
+        ))
+    }) {
+        device.write(&sequence)?;
+    }
+    Ok(())
 }
---
+++ readme.md
@@ -0,0 +1,9 @@
+Allows making the f-keys the primary keys without relying on the bloated logi+ software.
+
+Models (currently supported models are checked)
+- [*] k380
+- [*] k480
+
+Credits:
+- @jergusg Their repository was used to determine the k380's byte sequence, and the logic for how and when to send the sequence. https://github.com/jergusg/k380-function-keys-conf
+- @ericmbf Their repository was similarly used for the k480's byte sequence + device id. https://github.com/ericmbf/k480-ubuntu-fn-invert-auto
